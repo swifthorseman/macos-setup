83866424b521367acc405254b9704e61615dff7b
swifthorseman <swifthorseman@users.noreply.github.com>
2022-03-01 12:40:43 +0000

    Install Modern Unix commands using Homebrew

---
+++ README.md
@@ -0,0 +1,7 @@
+Modern Unix Homebrew Installs
+
+- Installs [Modern Unix](https://github.com/ibraheemdev/modern-Unix) using Homebrew.
+
+- Excludes `cheat`, `tldr`, `gtop` because there are no formulae for them. 
+
+
---
+++ brew_installs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+brew install bat
+brew install exa
+brew install lsd
+brew install git-delta
+brew install dust
+brew install duf
+brew install broot
+brew install fd
+brew install ripgrep
+brew install the_silver_searcher
+brew install fzf
+brew install mcfly
+brew install choose-rust
+brew install jq
+brew install sd
+brew install bottom
+brew install glances
+brew install hyperfine
+brew install gping
+brew install procs
+brew install httpie
+brew install rs/tap/curlie
+brew install xh
+brew install zoxide
+brew install dog
+
