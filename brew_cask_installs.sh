7d9682b3bf8252b7a5c504345a55a023cd83f40a
swifthorseman <swifthorseman@users.noreply.github.com>
2022-03-06 23:34:33 +0000

    Add iterm2 and vscode to Homebrew Cask

--- brew_cask_installs.sh
+++ brew_cask_installs.sh
@@ -9,6 +9,8 @@ brew install --cask drawio
 brew install --cask firefox
 brew install --cask font-hack-nerd-font
 brew install --cask istat-menus
+brew install --cask iterm2
 brew install --cask libreoffice
 brew install --cask protonvpn
+brew install --cask visual-studio-code
 brew install --cask zoom
