4eabed28f0b6eafa383019bf89759a37e067875e
swifthorseman <swifthorseman@users.noreply.github.com>
2022-03-06 20:26:54 +0000

    Add Homebrew package installation scripts

---
+++ brew_cask_installs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+brew tap homebrew/cask-fonts
+
+brew install --cask 1password
+brew install --cask atom
+brew install --cask authy
+brew install --cask drawio
+brew install --cask firefox
+brew install --cask font-hack-nerd-font
+brew install --cask istat-menus
+brew install --cask libreoffice
+brew install --cask protonvpn
+brew install --cask zoom
