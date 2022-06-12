8b70e3a50eb22bdc939a549ea3f81a966a3572e8
swifthorseman <swifthorseman@users.noreply.github.com>
2022-06-12 09:16:05 +0100

    Refactor cask installations

--- brew_cask_installs.sh
+++ brew_cask_installs.sh
@@ -1,20 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 brew tap homebrew/cask-fonts
 
-brew install --cask 1password
-brew install --cask atom
-brew install --cask authy
-brew install --cask brave-browser
-brew install --cask bluejeans
-brew install --cask dropbox
-brew install --cask drawio
-brew install --cask firefox
-brew install --cask font-hack-nerd-font
-brew install --cask istat-menus
-brew install --cask iterm2
-brew install --cask libreoffice
-brew install --cask mactex
-brew install --cask protonvpn
-brew install --cask visual-studio-code
-brew install --cask zoom
+while read LINE
+do brew install --cask $LINE
+done < brew_cask_packages.txt
