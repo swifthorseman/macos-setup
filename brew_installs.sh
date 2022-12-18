2b84bbe64fde54e488a125976b6a3232def15645
swifthorseman <swifthorseman@users.noreply.github.com>
2022-12-18 22:23:08 +0000

    Add coursier, jhead, openjdk8 and sbt

--- brew_installs.sh
+++ brew_installs.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+brew install coursier/formulas/coursier
 brew install gpg
+brew install jhead
 brew install kafka
 brew install kcat
 brew install minikube
+brew install openjdk@8
 brew install pipenv
+brew install sbt
 brew install structurizr-cli
 brew install tmux
 brew install tree
